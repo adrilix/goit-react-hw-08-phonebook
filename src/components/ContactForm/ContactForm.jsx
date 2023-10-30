1c1a5f6516f40db1f101141c390ba5e90e92eeed
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-10-30 22:09:25 +0100

    complete

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -39,7 +39,7 @@ function ContactForm(contacts) {
           type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Ba"
+          title="Name may contain only letters, apostrophe, dash and spaces."
           required
         />
       </LabelStyled>
